2bfc34aa06688fe2ce1d4c21c5b1988c014993fb
ElizabetHuaroto <huarotocardenas@gmail.com>
2023-04-18 22:04:48 -0500

    commit
    hasta levantar servicios

---
+++ data.js
@@ -0,0 +1,63 @@
+module.exports = function () {
+  var data = {
+    usuario: [
+      {
+        id:1,
+        nameUser: "Luis Pérez Reyes",
+        typeUser: "padre de familia",
+        emailUser:"lperez@gmail.com",
+        passwordUser: "123%&",
+        districUser:"San Juan de Lurigancho",
+        ageUser: 27
+      },
+
+      {
+        id:2,
+        nameUser: "Camilo andrade Guerra",
+        typeUser: "colegial",
+        emailUser:"Camilo@gmail.com",
+        passwordUser: "123%&",
+        districUser:"Los Olivos",
+        ageUser: 17
+      },
+      {
+        id:3,
+        nameUser: "Rosa del Campo Reyes",
+        typeUser: "padre de familia",
+        emailUser:"Rosa@gmail.com",
+        passwordUser: "123%&",
+        districUser:"San Isidro",
+        ageUser: 27
+      },
+      {
+        id:4,
+        nameUser: "Karina Cano Cardenas",
+        typeUser: "colegial",
+        emailUser:"karina@gmail.com",
+        passwordUser: "123%&",
+        districUser:"San Miguel",
+        ageUser: 14
+	},
+  {
+    id:5,
+    nameUser: "Luis Pérez Reyes",
+    typeUser: "Padre de Familia",
+    emailUser:"lperez@gmail.com",
+    passwordUser: "123%&",
+    districUser:"San Juan de Lurigancho",
+    ageUser: 27
+}
+    ],
+    colegio:[
+      {
+        id: 1,
+        nameSchool: "Trilce",
+	      districSchool: "LLos Olivos",
+        pointSchool: 5,
+       feeSchool: 300,
+       levelSchool:3
+      },
+    ]
+  }
+  return data
+}
