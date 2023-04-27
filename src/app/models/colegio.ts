74af3493d967b03731ea928836bdac32fbb9a19a
ElizabetHuaroto <huarotocardenas@gmail.com>
2023-04-27 10:25:34 -0500

    commit

--- src/app/models/colegio.ts
+++ src/app/models/colegio.ts
@@ -1,8 +1,7 @@
 export class Colegio
 {
   id:number=0;
-  nameBook: string="";
-  nHojas: number = 0;
-  publicationDateAuthor: Date = new Date(Date.now());
-  author: string="";
+  name: string="";
+  pension: number = 0;
+  district: string="";
 }
